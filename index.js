7d27da691398e6ac09a941d7a3e6f9e2e815a719
Chitrabhanu <chitabhanuy@gmail.com>
2022-12-22 12:42:05 +0530

    added jwt token

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors = require('cors');
 require('./db.js/config')
 const User = require("./db.js/User");
+
+const Jwt = require('jsonwebtoken');
+const jwtKey = 'e-comm'
+
 const app = express();
 
 app.use(express.json());
@@ -12,7 +16,13 @@ app.post("/register", async (req, resp) => {
     let result = await user.save();
     result = result.toObject();
     delete result.password
-    resp.send(result);
+    Jwt.sign({ result }, jwtKey, { expiresIn: "2h" }, (err, token) => {
+        if (err) {
+            resp.send({ result: "something went wrong please try after some time" })
+
+        }
+        resp.send({ result, auth: token })
+    })
 
 })
 
@@ -21,7 +31,14 @@ app.post("/login", async (req, resp) => {
     if (req.body.password && req.body.email) {
         let user = await User.findOne(req.body).select("-password");
         if (user) {
-            resp.send(user)
+            Jwt.sign({ user }, jwtKey, { expiresIn: "2h" }, (err, token) => {
+                if (err) {
+                    resp.send({ result: "something went wrong please try after some time" })
+
+                }
+                resp.send({ user, auth: token })
+            })
+
         } else {
             resp.send({ result: 'No user Found' })
         }
